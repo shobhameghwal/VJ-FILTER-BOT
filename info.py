28b6414e4719eb242c70adcbc7153e0d18aea8d2
Pankajmeghwal786 <166579228+Pankajmeghwal786@users.noreply.github.com>
2024-06-17 10:27:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,7 +67,7 @@ OWNER_USERNAME = environ.get('OWNER_USERNAME', 'kingvj01') # owner username with
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+y41v4I1V4zs3MDNl')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+y41v4I1V4zs3MDNl')
+CHNL_LNK = environ.get('CHNL_LNK', '@pankaj75_backup')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
 
